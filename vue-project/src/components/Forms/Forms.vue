f3324a976c98acf20ae4ea84b8b76331632c5e9d
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2024-11-11 23:12:24 +0330

    Working with v-model Modifiers and Numbers

--- vue-project/src/components/Forms/Forms.vue
+++ vue-project/src/components/Forms/Forms.vue
@@ -4,6 +4,13 @@
   <!-- Working with v-model Modifiers and Numbers -->
   <section class="tip">
     <h3>Working with v-model Modifiers and Numbers</h3>
+
+    <ul>
+      <li>
+        if the type of an input element is number the variable assigned to v-model is a type of
+        number. but the this.$refs.exampleInput.value is string
+      </li>
+    </ul>
   </section>
 </template>
 
