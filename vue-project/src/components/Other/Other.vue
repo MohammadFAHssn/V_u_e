990380d44a81526f2d94a0341f457f4a1fa2a4ed
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2024-11-13 14:57:44 +0330

    arrow function

--- vue-project/src/components/Other/Other.vue
+++ vue-project/src/components/Other/Other.vue
@@ -19,6 +19,19 @@ export default {}
     </ul>
   </section>
 
+  <!-- arrow function -->
+  <section class="tip">
+    <h3>arrow function</h3>
+
+    <div>
+      I get an error: "Cannot set property results of undefined". I'm getting this because "this"
+      keyword in here actually turns out to not refer to my view instance object unfortunately. The
+      easiest way of working around that is that we don't use this function syntax here but this
+      arrow function syntax instead: "this" keyword inside of them, does refer to the same context
+      as it does outside of them.
+    </div>
+  </section>
+
   <section class="tip">
     <p>@blur: when the input loses focus</p>
   </section>
