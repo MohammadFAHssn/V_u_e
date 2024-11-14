e62a450bd9cb473f5909536aa01187af5928607c
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2024-11-14 23:18:36 +0330

    History modes in vue router

--- vue-project/src/components/RoutingBuildingMultiPageSinglePageApplications/RoutingBuildingMultiPageSinglePageApplications.vue
+++ vue-project/src/components/RoutingBuildingMultiPageSinglePageApplications/RoutingBuildingMultiPageSinglePageApplications.vue
@@ -8,5 +8,18 @@ export default {}
   <!--  History modes in vue router -->
   <section class="tip">
     <h3>History modes in vue router</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org/guide/essentials/history-mode.html"
+          >Different History modes</a
+        >
+      </li>
+      <li>
+        <img src="/src/assets/images/catch-all-fallback-route-vite.png" width="800px" />
+      </li>
+      <li>
+        <img src="/src/assets/images/catch-all-fallback-route-laravel.png" width="800px" />
+      </li>
+    </ul>
   </section>
 </template>
