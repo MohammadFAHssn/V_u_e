24ffd14823d543fc41cbdfd4b8f387cb0cec3187
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-04-19 12:37:52 +0330

    The Composition API - Replacing the Options API, Which Problem Does The Composition API Solve

---
+++ vue-project/src/components/TheCompositionAPI_ReplacingTheOptionsAPI/TheCompositionAPI_ReplacingTheOptionsAPI.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <h2>The Composition API - Replacing the Options API</h2>
+
+  <!-- Which Problem Does The Composition API Solve -->
+  <section class="tip">
+    <h3>Which Problem Does The Composition API Solve</h3>
+
+    <p>
+      <img src="/src/assets/images/option-api.png" width="300px" />
+    </p>
+
+    <p>in option api, re-using logic across components can also be tricky or cumbersome</p>
+
+    <p>
+      <img
+        src="/src/assets/images/Which-Problem-Does-The-Composition-API-Solve.png"
+        width="600px"
+      />
+    </p>
+  </section>
+
+  <span></span>
+</template>
