27aea2e5f2d7f20c6630e52bd53d7f64fd7ba9ba
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-04-20 10:38:07 +0330

    Replacing data with refs

--- vue-project/src/components/TheCompositionAPI_ReplacingTheOptionsAPI/TheCompositionAPI_ReplacingTheOptionsAPI.vue
+++ vue-project/src/components/TheCompositionAPI_ReplacingTheOptionsAPI/TheCompositionAPI_ReplacingTheOptionsAPI.vue
@@ -37,7 +37,63 @@ export default {}
     </p>
 
     <p>The only other part which will also change will be the lifecycle methods</p>
+
+    <p>
+      <strong>ref</strong> creates a so-called reference, but now not just a reference to some DOM
+      element, but instead, a value which we can reference. So ref creates a value, but not any kind
+      of value, Instead a reactive value. where Vue will find out when we change it. the const
+      varaible that is assigned to the ref function is an object.
+    </p>
+
+    <p>into return object, we'll put all the things, we wanna expose to the template</p>
+
+    <p>
+      Now, actually you wouldn't even need the ref method, if I remove it and I store a normal
+      string here, then all the words. but it is not reactive anymore.
+    </p>
+
+    <p>
+      <img src="/src/assets/images/ref.png" width="300px" />
+    </p>
+
+    <div class="tip">
+      <p>
+        Vue.js offers an alternative syntax:&nbsp;You can use
+        <code>&lt;script setup&gt;</code> instead of manually adding a
+        <code>setup()</code>&nbsp;method.
+      </p>
+      <p>The following code:</p>
+      <pre class="tip">
+&lt;script&gt;
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const uName =&nbsp;ref('Maximilian');
+
+    setTimeout(function()&nbsp;{
+      uName.value =&nbsp;'Max';
+    }, 2000);
+
+    return { uName };
+  }
+}
+&lt;/script&gt;</pre
+      >
+      <p>could be replaced with:</p>
+      <pre class="tip">
+&lt;script setup&gt;
+import { ref } from 'vue';
+
+const uName =&nbsp;ref('Maximilian');
+
+setTimeout(function()&nbsp;{
+  uName.value =&nbsp;'Max';
+}, 2000);
+&lt;/script&gt;</pre
+      >
+    </div>
   </section>
 
-  <span></span>
+  <span> </span>
 </template>
